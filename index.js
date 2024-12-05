ad0e2c4ade6745453d927a3eda0e2aa1ce2e868b
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2024-12-05 16:48:53 +0300

    Saving changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const maine=document.getElementById('main');
+maine.remove();
+
+const newHeader=document.createElement('h1');
+
+newHeader.id='victory'
+
+newHeader.textContent="Tamara is the champion"
